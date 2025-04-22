5f675c4678935415ad5ff72dabde242896d257f0
Nicholas Barros <81240462+NickRyanDev@users.noreply.github.com>
2025-04-21 21:05:27 -0300

    Add files via upload

--- Blitao/Blitao.ino
+++ Blitao/Blitao.ino
@@ -1,14 +1,16 @@
+//include Libraries
+#include "DFRobotDFPlayerMini.h"
+#include <SoftwareSerial.h>
 //Shield Pins
-const int EN0 = 12;
-const int EN1 = 13;
-const int motor0B = 4;
-const int motor0F = 5;
-const int motor1B = 6;
-const int motor1F = 7;
-const int motor2B = 8;
-const int motor2F = 9;
-const int motor3B = 10;
-const int motor3F = 11;
+const int EN0 = 18;
+const int EN1 = 19;
+const int motor0B = 7;
+const int motor0F = 8;
+const int motor1B = 12;
+const int motor1F = 13;
+const int motor2B = 14;
+const int motor2F = 15;
+const int sawMotor = 16;
 //Bluethooth Control
 char command;
 #define FORWARD 'F'
@@ -20,6 +22,12 @@ char command;
 #define CROSS 'X'
 #define TRIANGLE 'T'
 #define SQUARE 'S'
+//Player Pins
+const int pinRX = 10;
+const int pinTX = 11;
+//Instance Player Variables
+SoftwareSerial serial(pinRX, pinTX);
+DFRobotDFPlayerMini player;
 
 void setup() {
   pinMode(motor0B, OUTPUT);
@@ -28,21 +36,47 @@ void setup() {
   pinMode(motor1F, OUTPUT);
   pinMode(motor2B, OUTPUT);
   pinMode(motor2F, OUTPUT);
-  pinMode(motor3B, OUTPUT);
-  pinMode(motor3F, OUTPUT);
+  pinMode(sawMotor, OUTPUT);
   //Iniciate motors
   digitalWrite(EN0, OUTPUT);
   digitalWrite(EN1, OUTPUT);
-  //Iniciate Serial Communication
+  //Iniciate Bluethooth Serial Communication
   Serial.begin(9600);
+  //Iniciate DFPlayer Serial Communication and set volume
+  serial.begin(9600);
+  player.volume(28);
 }
 
 void loop() {
   if (Serial.available()){
     command = Serial.read();
     switch (command){
+      //Moves
       case FORWARD:
         forwardTwoMotors(motor0B, motor0F, motor1B, motor1F);
+        break;
+      case BACKWARD:
+        backwardTwoMotors(motor0B, motor0F, motor1B, motor1F);
+        break;
+      case LEFT:
+        turnLeft(motor0B, motor0F, motor1B, motor1F);
+        break;
+      case RIGHT:
+        turnRight(motor0B, motor0F, motor1B, motor1F);
+        break;
+      //weapon
+      case TRIANGLE:
+        attack(sawMotor, motor2B, motor2F);
+        break;
+      case CROSS:
+        backAttack(sawMotor, motor2B, motor2F);
+        break;
+      //Ornaments
+      case SQUARE:
+        player.play(1);
+        break;
+      default:
+        standing(motor0B, motor0F, motor1B, motor1F);
         break;
     }
   }
@@ -69,11 +103,34 @@ void turnRight(int pin0, int pin1, int pin3, int pin4){
   forward(pin0, pin1);
   backward(pin3, pin4);
 }
-void forwardTwoMotors(int pin0, int pin1, int pin3, int pin4){
-  forward(pin0, pin1);
-  forward(pin3, pin4);
+void forwardTwoMotors(int pin0, int pin3){
+  digitalWrite(pin0, HIGH);
+  digitalWrite(pin3, HIGH);
 }
-void backwardTwoMotors(int pin0, int pin1, int pin3, int pin4){
+void backwardTwoMotors(int pin1, int pin4){
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin4, HIGH);
+}
+void standing(int pin0, int pin1, int pin3, int pin4){
+  digitalWrite(pin0, LOW);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
+}
+//Turn on saw's motor
+void sawOn(int sawPin){
+  digitalWrite(sawPin, HIGH);
+}
+void sawOff(int pisawPinn){
+  digitalWrite(sawPin, LOW);
+}
+
+//Weapons attack
+void attack(int sawPin, int pin0, int pin1){
+  sawOn(sawPin);
+  forward(pin0, pin1);
+}
+void backAttack(int sawPin, int pin0, intpin1){
+  sawOff(sawPin);
   backward(pin0, pin1);
-  backward(pin3, pin4);
 }
